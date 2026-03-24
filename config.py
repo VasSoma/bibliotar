76f4926dc8822c9d73fb689e61369ea158b6ffd9
FZsolt778 <fzsolt778@gmail.com>
2026-03-24 11:52:51 +0100

    add class: Config

---
+++ config.py
@@ -0,0 +1,5 @@
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.gdqijunubhvvpnqmbjkk:ParajdiSo45@aws-1-eu-west-1.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
