f4539d33aa2c1b6a9dc154a9489d55dfab83c64a
FZsolt778 <fzsolt778@gmail.com>
2026-03-24 20:59:24 +0100

    add class: models/role.py

---
+++ backend/models/role.py
@@ -0,0 +1,13 @@
+from sqlalchemy import ForeignKey
+
+
+from ..extensions import db, Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.types import String,Integer
+from typing import List,Optional
+
+class Role(db.Model):
+    __tablename__ = "roles"
+
+    role_id : Mapped[int] = mapped_column(primary_key=True)
+    role_name : Mapped[str] = mapped_column(String(100))
