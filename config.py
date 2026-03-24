285946529722cfc231e3ace30af646b76f2b4138
FZsolt778 <fzsolt778@gmail.com>
2026-03-24 12:39:33 +0100

    Update: track_modifications = true

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.gdqijunubhvvpnqmbjkk:ParajdiSo45@aws-1-eu-west-1.pooler.supabase.com:6543/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
