ec6ff7148c487e06f657a3b5bebe57c050e0a469
FZsolt778 <fzsolt778@gmail.com>
2026-03-24 20:41:29 +0100

    update: models/user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,14 +1,19 @@
+
+
+from sqlalchemy import ForeignKey
+
 from ..extensions import db, Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.types import String,Integer
 from typing import List,Optional
 
 class User(db.Model):
-    __tablename__ : "users"
+    __tablename__ = "users"
 
     user_id : Mapped[int] = mapped_column(primary_key=True)
     role : Mapped[str] = mapped_column(String(20))
     password_hashed : Mapped[str] = mapped_column(String(255))
     phone_number : Mapped[Optional[str]] = mapped_column(String(15))
-    address : Mapped[str] = mapped_column(String(100))
     name : Mapped[str] = mapped_column(String(100)) 
+
+    address_id : Mapped[int] = mapped_column(ForeignKey("home_address.address_id"))
