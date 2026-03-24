dc6632f8413ac52276820e939ec94f5791ac4903
FZsolt778 <fzsolt778@gmail.com>
2026-03-24 16:21:41 +0100

    update comments: __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,15 +2,16 @@ from flask import Flask
 from config import Config
 from .extensions import db
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+def create_app(config_class=Config): #got a config.py
+    app = Flask(__name__) #gen flask obj
+    app.config.from_object(config_class) # setup flask with config.py
 
-    db.init_app(app)
+    db.init_app(app) # flask + database connection
+
     from flask_migrate import Migrate
-    migrate = Migrate(app,db)
+    migrate = Migrate(app,db) # helper, modify database if smt changed
 
     from .main import bp
-    app.register_blueprint(bp)
+    app.register_blueprint(bp) # regist blueprint with bp
 
     return app
