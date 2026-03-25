923ecbd44f4f74768a9132f47efb9fa920befaa5
FZsolt778 <fzsolt778@gmail.com>
2026-03-25 16:06:42 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from config import Config
 from backend import create_app
 
 if __name__ =="__main__":
-    create_app(config_class=Config).run('localhost',8888)
+    create_app(config_class=Config).run('localhost',8888) 
