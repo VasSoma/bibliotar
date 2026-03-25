13206cdea3f1bd1382881a18ac95c08f6e17500c
FZsolt778 <fzsolt778@gmail.com>
2026-03-25 22:51:02 +0100

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from apiflask import APIFlask
 from .config import Config
 from .extensions import db
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+def create_app(config_class=Config):
+    app = APIFlask(__name__,json_errors=True,title="Bibliotar_API",docs_path="/swagger") #generate apiflask obj
+    app.config.from_object(config_class) # setup flask with config.py
 
-    db.init_app(app)
+    db.init_app(app) # flask + database connection
+
+    # from flask_migrate import Migrate
+    # migrate = Migrate(app,db) # helper, modify database if smt changed
+
+    from .blueprint import bp as main_bp
+    app.register_blueprint(main_bp, url_prefix='/api')
 
     @app.route("/")
     def hello_world():
