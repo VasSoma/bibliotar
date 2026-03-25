e9f3637b686d182f65892bf0ec0ecf4fc224e42c
FZsolt778 <fzsolt778@gmail.com>
2026-03-25 18:11:45 +0100

    add: APIflask/__init__.py

---
+++ backend/APIflask/__init__.py
@@ -0,0 +1,17 @@
+from apiflask import APIFlask
+from config import Config
+from .extensions import db
+
+def create_app(config_class=Config): #got a config.py
+    app = APIFlask(__name__,json_errors=True,title="Bibliotar_API") #gen flask obj
+    app.config.from_object(config_class) # setup flask with config.py
+
+    db.init_app(app) # flask + database connection
+
+    from flask_migrate import Migrate
+    migrate = Migrate(app,db) # helper, modify database if smt changed
+
+    from ..main import bp
+    app.register_blueprint(bp) # regist blueprint with bp
+
+    return app
