ff88f707a2da44a168a30a37628d9effd4bb696a
FZsolt778 <fzsolt778@gmail.com>
2026-03-25 20:00:04 +0100

    Update role.py

--- backend/models/role.py
+++ backend/models/role.py
@@ -1,7 +1,7 @@
 from sqlalchemy import ForeignKey
 from .user import UserRole
 
-from ..extensions import db, Base
+from ..APIflask.extensions import db, Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.types import String,Integer
 from typing import List,Optional
