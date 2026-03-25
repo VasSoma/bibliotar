6b313dcdfeebe0e265bad2ae009ab213bb8f8a7b
FZsolt778 <fzsolt778@gmail.com>
2026-03-25 18:11:19 +0100

    move: extensions.py

--- backend/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase): #default class, work with instantiation 
-    pass
-
-db = SQLAlchemy(model_class = Base) # given to sqlalchemy
