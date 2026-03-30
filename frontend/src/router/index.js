df309c13db2e8d4cc95af9a64136d46a8b65fe36
patkosboglarka <patkosboglarka08@gmail.com>
2026-03-30 23:17:54 +0200

    books.page and modify login page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,6 +13,11 @@ const router = createRouter({
       name: 'login',
       component: () => import('../pages/Login.page.vue'),
     },
+    {
+      path: '/books',
+      name: 'books',
+      component: () => import('../pages/Books.page.vue'),
+    },
   ],
 })
 
