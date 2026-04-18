54038031fff611ba2b7d084c0dc0f23901faae17
VasSoma <vasmaticssoma@gmail.com>
2026-04-18 14:55:47 +0200

    Move all auth related endpoints from user/routes to auth/routes

--- backend/app/blueprint/auth/schemas.py
+++ backend/app/blueprint/auth/schemas.py
@@ -1,5 +1,8 @@
+from __init__ import bp
 from marshmallow import Schema, fields
 from apiflask.validators import Email
+from apiflask import HTTPError
+
 
 class LoginRequestSchema(Schema):
     email = fields.String(validate=Email)
