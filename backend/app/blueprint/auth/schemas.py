c2d3626974dff358ea4f745483a0b968491a7f99
VasSoma <vasmaticssoma@gmail.com>
2026-04-18 14:10:37 +0200

    Add validation for email

--- backend/app/blueprint/auth/schemas.py
+++ backend/app/blueprint/auth/schemas.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields
+from apiflask.validators import Email
 
 class LoginRequestSchema(Schema):
-    email = fields.String()
+    email = fields.String(validate=Email)
     password = fields.String()
