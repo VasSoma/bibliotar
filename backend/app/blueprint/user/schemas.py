d8adce7dbe2672f3448d2dcc0b4495e28b1e7e26
FZsolt778 <fzsolt778@gmail.com>
2026-04-18 20:50:44 +0200

    Update schemas.py

--- backend/app/blueprint/user/schemas.py
+++ backend/app/blueprint/user/schemas.py
@@ -1,24 +1,32 @@
-from marshmallow import Schema,fields
-from apiflask.validators import Email
+from marshmallow import Schema, fields
+
 
 class AddressSchema(Schema):
+    county = fields.String()
+    postal_code = fields.String()
     city = fields.String()
     street = fields.String()
-    country = fields.String()
-    postal_code = fields.String()
     house_number = fields.String()
 
-class UserRequestSchema(Schema):
-    password_hashed = fields.String()
-    phone_number = fields.String()
+
+class UserResponseSchema(Schema):
+    user_id = fields.Integer()
+    role = fields.String()
     name = fields.String()
-    email = fields.String(validate=Email())
+    email = fields.String()
+    phone_number = fields.String()
     address = fields.Nested(AddressSchema)
 
-class UserResponseSchema(Schema): #API response
-    id = fields.Integer()
-    role = fields.String()
-    phone_number = fields.String()
+
+class ProfileUpdateRequestSchema(Schema):
+    name = fields.String(required=True)
+    email = fields.String(required=True)
+    phone_number = fields.String(required=True)
+    address = fields.Nested(AddressSchema, required=True)
+
+
+class ProfileUpdateResponseSchema(Schema):
     name = fields.String()
-    email = fields.String(validate=Email)
+    email = fields.String()
+    phone_number = fields.String()
     address = fields.Nested(AddressSchema)
