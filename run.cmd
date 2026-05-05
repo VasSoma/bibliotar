9ffc32c03fef3ba0350bb94d60ffb2b647a717c4
FZsolt778 <fzsolt778@gmail.com>
2026-05-05 08:54:49 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,5 +1,4 @@
-python %cd%\backend\app.py
-
-cd %cd%\frontend\
-npm install
-npm run dev
+@echo off
+set dict=%~dp0
+start "Backend" cmd /k "python %dict%backend\run.py"
+start "Frontend" cmd /k "cd /d %dict%frontend && npm install && npm run dev"
