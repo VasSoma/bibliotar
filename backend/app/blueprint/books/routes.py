ec3c31a478ab36bff5b62137c1104ea492d61c5c
patkosboglarka <patkosboglarka08@gmail.com>
2026-05-06 15:52:19 +0200

    Fix book endpoint, add search query param handling

--- backend/app/blueprint/books/routes.py
+++ backend/app/blueprint/books/routes.py
@@ -1,38 +1,40 @@
 from . import bp
 from .schemas import BookRequestSchema
 from .schemas import BookResponseSchema
+from .schemas import BookQuerySchema
 from .service import BookService
 from .. import role_required
 from ...extensions import auth
 
-@bp.get('/')
+@bp.get('')
+@bp.input(BookQuerySchema, arg_name="query", location="query")
 @bp.output(BookResponseSchema(many=True))
-def get_books():
-    return BookService.get_books()
+def get_books(query):
+    return BookService.get_books(query["search"])
 
 @bp.get('/<int:book_id>')
 @bp.output(BookResponseSchema)
 def get_book_by_id(book_id):
     return BookService.get_book_by_id(book_id)
 
-@bp.post('/new')
+@bp.post('')
 @bp.input(BookRequestSchema)
 @bp.output(BookResponseSchema)
 @bp.auth_required(auth)
-# @role_required(["librarian"])
+@role_required(["admin"])
 def create_book(json_data):
     return BookService.create_book(json_data)
 
-@bp.patch('update')
+@bp.patch('/<int:book_id>')
 @bp.input(BookRequestSchema)
 @bp.output(BookResponseSchema)
 @bp.auth_required(auth)
-@role_required(["librarian"])
+@role_required(["admin"])
 def update_book(json_data):
     return BookService.update_book(json_data)
 
 @bp.delete('/<int:book_id>')
 @bp.auth_required(auth)
-@role_required(["librarian"])
+@role_required(["admin"])
 def delete_book(book_id):
     return BookService.delete_book(book_id)
