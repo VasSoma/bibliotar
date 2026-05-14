ca4adc60de6edf26822dc212a48b8d1130463762
FZsolt778 <fzsolt778@gmail.com>
2026-05-14 10:13:43 +0200

    for new book all field is required

--- backend/app/blueprint/books/schemas.py
+++ backend/app/blueprint/books/schemas.py
@@ -2,11 +2,11 @@ from apiflask import APIBlueprint
 from marshmallow import Schema, fields
 
 class BookRequestSchema(Schema):
-    title = fields.String()
-    isbn = fields.String()
-    quantity = fields.Integer()
-    is_available = fields.Boolean()
-    author = fields.String()
+    title = fields.String(required=True)
+    isbn = fields.String(required=True)
+    quantity = fields.Integer(required=True)
+    is_available = fields.Boolean(required=True)
+    author = fields.String(required=True)
 
 class BookResponseSchema(Schema):
     book_id = fields.Integer()
